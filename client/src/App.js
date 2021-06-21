15a9b8d2cb4f4fe9190a679232333b41b9f87ea9
Rohit Anand <rrohitanand3336@gmail.com>
2021-06-21 11:53:55 +0530

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Footer from './Footer/Footer';
+import Main from './Main/Main';
+import Menu from './Menu/Menu';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Menu />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/Menu/Menu.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Menu.css'
+
+const Menu = () => {
+      return (
+            <>
+                  <div className="header">
+                        <h1>PortFolio</h1>
+                  </div>
+            </>
+      )
+}
+
+export default Menu
